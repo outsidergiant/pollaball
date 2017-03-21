d6919f3c0496ad467ef4ae8ce0375edc0c58f502
Paul <sirdjpavline@yandex.ru>
2017-03-22 02:55:36 +0300

    Unity Project Commit

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rigidBody;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorisontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorisontal, 0f, moveVertical);
+        rigidBody.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 11)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
